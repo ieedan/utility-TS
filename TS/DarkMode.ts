16ff18ea2fa538935a54d80983a72949ceb5f1e8
Aidan Bleser <aidanbleser35@gmail.com>
2023-09-22 11:13:09 -0500

    Update DarkMode.ts

--- TS/DarkMode.ts
+++ TS/DarkMode.ts
@@ -8,19 +8,26 @@
  * ``` 
  */
 export enum ColorPreference {
-    dark = 'dark',
-    light = 'light',
-    OS = 'OS'
+	dark = "dark",
+	light = "light",
+	OS = "OS",
 }
 
 /** Configures preferred color scheme
- * 
+ *
  */
 const loadDarkMode = (): void => {
-	if (
-		localStorage.theme === "dark" ||
-		(!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-	) {
+	const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+	updateMode();
+
+	colorSchemeQuery.addEventListener("change", updateMode);
+};
+
+const updateMode = () => {
+	const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+	if (localStorage.theme === "dark" || (!("theme" in localStorage) && colorSchemeQuery.matches)) {
 		document.documentElement.classList.add("dark");
 	} else {
 		document.documentElement.classList.remove("dark");
@@ -28,39 +35,29 @@ const loadDarkMode = (): void => {
 };
 
 /** Gets the current color scheme preference
- * 
- * @returns 
+ *
+ * @returns
  */
 const getCurrentPreference = (): ColorPreference => {
-    if (localStorage.theme === "dark") {
-        return ColorPreference.dark;
-    }
+	if (localStorage.theme === "dark") return ColorPreference.dark;
 
-    if (localStorage.theme === "light") {
-        return ColorPreference.light;
-    }
+	if (localStorage.theme === "light") return ColorPreference.light;
 
-    return ColorPreference.OS;
-}
+	return ColorPreference.OS;
+};
 
 /** Changes the current color scheme preference
- * 
- * @param preference 
+ *
+ * @param preference
  */
 const changePreference = (preference: ColorPreference) => {
-    if (preference === ColorPreference.OS) {
-        localStorage.removeItem("theme");
-    }
+	if (preference === ColorPreference.OS) localStorage.removeItem("theme");
 
-    if (preference === ColorPreference.light) {
-        localStorage.theme = "light";        
-    }
+	if (preference === ColorPreference.light) localStorage.theme = "light";
 
-    if (preference === ColorPreference.dark) {
-        localStorage.theme = 'dark';
-    }
+	if (preference === ColorPreference.dark) localStorage.theme = "dark";
 
-    loadDarkMode();
+	loadDarkMode();
 };
 
 export { loadDarkMode, changePreference, getCurrentPreference };
